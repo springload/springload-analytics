b7313a2141874c89f30c97d703e4cdc54bb4bd78
Yuzi Ma <yuzi.ma@springload.co.nz>
2021-03-02 15:58:57 +1300

    Forward ref and other props

--- src/EventBoundary.tsx
+++ src/EventBoundary.tsx
@@ -10,35 +10,45 @@ interface IEventBoundaryProps {
   sendTo?: string[];
 }
 
-const EventBoundary = ({
-  triggerEvent = 'click',
-  eventName,
-  payload,
-  sendTo,
-  disabled = false,
-  children,
-}: IEventBoundaryProps): JSX.Element => {
-  return (
-    <React.Fragment>
-      {React.Children.map(children, (element) => {
-        if (!React.isValidElement(element)) return;
+const EventBoundary = React.forwardRef(
+  (
+    {
+      triggerEvent = 'click',
+      eventName,
+      payload,
+      sendTo,
+      disabled = false,
+      children,
+      ...rest
+    }: IEventBoundaryProps,
+    ref
+  ): JSX.Element => {
+    return (
+      <React.Fragment>
+        {React.Children.map(children, (element) => {
+          if (!React.isValidElement(element)) return;
 
-        const eventPropName = `on${
-          triggerEvent.charAt(0).toUpperCase() + triggerEvent.slice(1)
-        }`;
-        const eventHandler = (event: React.SyntheticEvent) => {
-          if ((element.props as any)[eventPropName]) {
-            (element.props as any)[eventPropName](event);
-          }
-          payload = typeof payload === 'function' ? payload() : payload;
-          if (!disabled) {
-            track(eventName, payload, sendTo);
-          }
-        };
-        return React.cloneElement(element, { [eventPropName]: eventHandler });
-      })}
-    </React.Fragment>
-  );
-};
+          const eventPropName = `on${
+            triggerEvent.charAt(0).toUpperCase() + triggerEvent.slice(1)
+          }`;
+          const eventHandler = (event: React.SyntheticEvent) => {
+            if ((element.props as any)[eventPropName]) {
+              (element.props as any)[eventPropName](event);
+            }
+            payload = typeof payload === 'function' ? payload() : payload;
+            if (!disabled) {
+              track(eventName, payload, sendTo);
+            }
+          };
+          return React.cloneElement(element, {
+            [eventPropName]: eventHandler,
+            ref,
+            ...rest
+          } as any);
+        })}
+      </React.Fragment>
+    );
+  }
+);
 
 export default EventBoundary;
