d8694265b24d0da3c518a4394caa4f913ed52b69
Phil <phil@springload.co.nz>
2021-05-24 13:20:59 +1200

    use promise with perfume import

--- src/plugins/performance-plugin.ts
+++ src/plugins/performance-plugin.ts
@@ -3,21 +3,21 @@ import perfumePlugin from '@analytics/perfumejs';
 // Track performance metrics
 const performancePlugin = ({ sendTo }: { sendTo?: string[] }) => {
   if (typeof window !== 'undefined') {
-    const Perfume = import('perfume.js');
+    import('perfume.js').then((Perfume) => {
+      const destinations = Array.isArray(sendTo)
+        ? sendTo.reduce((acc, tracker) => ({ ...acc, [tracker]: true }), {})
+        : { all: true };
 
-    const destinations = Array.isArray(sendTo)
-      ? sendTo.reduce((acc, tracker) => ({ ...acc, [tracker]: true }), {})
-      : { all: true };
-
-    return perfumePlugin({
-      perfume: Perfume,
-      category: 'perfMetrics',
-      destinations,
-      perfumeOptions: {
-        resourceTiming: true,
-        elementTiming: true,
-        maxMeasureTime: 15000,
-      },
+      return perfumePlugin({
+        perfume: Perfume,
+        category: 'perfMetrics',
+        destinations,
+        perfumeOptions: {
+          resourceTiming: true,
+          elementTiming: true,
+          maxMeasureTime: 15000,
+        },
+      });
     });
   }
 };
